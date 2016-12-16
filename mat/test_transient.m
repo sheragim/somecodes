175895cf792cb6fb9684153bfe1b78c961f36e37
Shahrouz <shahrouz@imex.ucsd.edu>
2016-12-16 12:47:39 -0800

    cichyp.py finished, and bounds is working on

---
+++ mat/test_transient.m
@@ -0,0 +1,10 @@
+xx= load('data1.txt');
+x1=xx(1:10^4);
+% x1=xx(1:,log(1+length(xx))*length(xx)/1000)
+plot(x1)
+
+ind1 = transient_time_detector(x1)
+
+figure(5);clf
+plot(x1); hold on
+plot([ind1,ind1],[min(x1)/2, max(x1)], '--r')
